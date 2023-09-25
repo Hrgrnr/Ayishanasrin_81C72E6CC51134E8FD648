6fe4fbc128ffb855d0e2461ff6a9ad227b442657
Hrgrnr <145996002+Hrgrnr@users.noreply.github.com>
2023-09-25 21:21:04 +0530

    Add files via upload

---
+++ main (1.2).py
@@ -0,0 +1,16 @@
+# 1.2 write a program that determine whether a year entered by the user is a leap year or not using if-elif-else statements
+
+def isleapyear(year):
+  if (year % 4 == 0 and year %100 != 0) or year %400 ==0:
+    return True
+  else:
+    return False
+
+year = 2012
+
+if isleapyear(year):
+  print('{} is a leap year.'.format(year))
+else:
+  print('{} is not a leap year.'.format(year))
+  
+     
